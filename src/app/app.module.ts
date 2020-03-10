615672b6c985241c1604715e9e10594e8906d82d
Amritpal Singh <aamritpa@sfu.ca>
2020-03-09 19:52:32 -0700

    recipe list started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,29 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import {HeaderComponent} from  './header/header.component';
+import { RecipeEditComponent } from './shopping-list/recipe-edit/recipe-edit.component';
+import {RecipeDetailComponent} from './recipe/recipe-detail/recipe-detail.component';
+import {RecipeListComponent} from './recipe/recipe-list/recipe-list.component'
+import {RecipeItemComponent} from './recipe/recipe-list/recipe-item/recipe-item.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipeComponent,
+    HeaderComponent,
+    RecipeEditComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('Test','Simply atest','https://i.ndtvimg.com/i/2015-10/urlai-roast_625x350_71444723419.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
